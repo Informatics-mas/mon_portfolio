3bf65451759f6a13e06ff2df7036bfdb90077e87
informatics-mas <ogouogoudavid@gmail.com>
2026-01-30 01:26:26 +0000

    modification de la page web

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function hamburg() {
 function cancel() {
     document.querySelector(".dropdown").style.display = "none";
 }
+
+function downloadPDF() {
+    window.location.href = "https://drive.google.com/file/d/1DuVa9YQIQrJPAn7LcZC9ae3YJJCX4Jlm/view?usp=drivesdk";
+}
