51b5c8ff7dcdcfcab187b702b56f0a3a3d21e50b
informatics-mas <ogouogoudavid@gmail.com>
2026-01-30 01:47:22 +0000

    modification de la page web

--- script.js
+++ script.js
@@ -7,5 +7,22 @@ function cancel() {
 }
 
 function downloadPDF() {
-    window.location.href = "https://drive.google.com/file/d/1DuVa9YQIQrJPAn7LcZC9ae3YJJCX4Jlm/view?usp=drivesdk";
+  const msg = document.getElementById("message");
+
+  // Afficher le message
+  msg.style.display = "block";
+
+  // Cacher le message après 3s
+  setTimeout(() => {
+    msg.style.display = "none";
+  }, 3000);
+
+  // Téléchargement automatique
+  const link = document.createElement("a");
+  link.href = "https://drive.google.com/file/d/1DuVa9YQIQrJPAn7LcZC9ae3YJJCX4Jlm/view?usp=drivesdk";
+  link.download = "CV-Davidson.pdf";
+
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
